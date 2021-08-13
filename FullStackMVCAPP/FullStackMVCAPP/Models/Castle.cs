d6585e0530d070675482da0b25410fafd3d5669e
sc17sp <sean.parker@x-labsystems.co.uk>
2021-08-13 13:52:04 +0100

    Castle Db Context method added

--- FullStackMVCAPP/FullStackMVCAPP/Models/Castle.cs
+++ FullStackMVCAPP/FullStackMVCAPP/Models/Castle.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
 namespace FullStackMVCAPP.Models
 {
+    public class castleContext : DbContext
+    { 
+        public DbSet<Castle> Castles { get; set; }    
+    }
     public class Castle
     {
         public int Id { get; set; }
