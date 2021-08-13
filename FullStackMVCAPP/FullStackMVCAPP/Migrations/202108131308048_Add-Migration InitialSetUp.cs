b7f1f0d63f9affc5c21273815e5d65b47edbc4f7
sc17sp <sean.parker@x-labsystems.co.uk>
2021-08-13 14:50:08 +0100

    Migrated to intially created database, added seed data to configuration file

---
+++ FullStackMVCAPP/FullStackMVCAPP/Migrations/202108131308048_Add-Migration InitialSetUp.cs
@@ -0,0 +1,43 @@
+﻿namespace FullStackMVCAPP.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddMigrationInitialSetUp : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Castles",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Houses",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                        Region = c.String(nullable: false),
+                        Words = c.String(),
+                        CastleId_Id = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Castles", t => t.CastleId_Id, cascadeDelete: true)
+                .Index(t => t.CastleId_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Houses", "CastleId_Id", "dbo.Castles");
+            DropIndex("dbo.Houses", new[] { "CastleId_Id" });
+            DropTable("dbo.Houses");
+            DropTable("dbo.Castles");
+        }
+    }
+}
---
+++ FullStackMVCAPP/FullStackMVCAPP/Migrations/Configuration.cs
@@ -0,0 +1,76 @@
+﻿namespace FullStackMVCAPP.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<FullStackMVCAPP.Models.GOTContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(FullStackMVCAPP.Models.GOTContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+            context.Castles.AddOrUpdate(x => x.Id,
+                new Models.Castle() { Id = 1, Name = "Winterfell" },
+                new Models.Castle() { Id = 2, Name = "Pyke" },
+                new Models.Castle() { Id = 3, Name = "Casterly Rock" },
+                new Models.Castle() { Id = 4, Name = "The Eyrie" },
+                new Models.Castle() { Id = 5, Name = "The Twins" },
+                new Models.Castle() { Id = 6, Name = "DragonStone" }
+                );
+
+            context.Houses.AddOrUpdate(x => x.Id,
+                new Models.House() {
+                    Id = 1,
+                    Name = "Arryn",
+                    Region = "The Vale of Arryn",
+                    Words = "As High as Honor",
+                    CastleId = context.Castles.Find(4) 
+                },
+                new Models.House() {
+                    Id = 2,
+                    Name = "Greyjoy",
+                    Region = "Iron Islands",
+                    Words = "What is Dead May Never Die",
+                    CastleId = context.Castles.Find(2) 
+                },
+                new Models.House() {
+                    Id = 3,
+                    Name = "Lannister",
+                    Region = "The Western Islands",
+                    Words = "A Lannister Always Pays His Debts",
+                    CastleId = context.Castles.Find(3)
+                },
+                new Models.House() { 
+                    Id = 4,
+                    Name = "Stark",
+                    Region = "The North",
+                    Words = "Winter is Coming",
+                    CastleId = context.Castles.Find(1)
+                },
+                new Models.House() { 
+                    Id = 5,
+                    Name = "Targaryen",
+                    Region = "The Crownlands",
+                    Words = "Fire and Blood",
+                    CastleId = context.Castles.Find(6)
+                },
+                new Models.House() { 
+                    Id = 6,
+                    Name = "Frey",
+                    Region = "The Riverlands",
+                    Words = "We Stand together",
+                    CastleId = context.Castles.Find(5)
+                }
+                );
+        }
+    }
+}
