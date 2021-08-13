54094013da8b8a5ec887aef7887a9aba2f86829b
sc17sp <sean.parker@x-labsystems.co.uk>
2021-08-13 13:47:29 +0100

    Added House Model properties and data annotations

---
+++ FullStackMVCAPP/FullStackMVCAPP/Models/Houses.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FullStackMVCAPP.Models
+{
+    public class Houses
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Region { get; set; }
+        public string Words { get; set; }
+        [Required]
+        public virtual Castles castleId { get; set; }
+    }
+}
