9d337a95effb17c4702cc4960ae40fdfcc38e68e
sc17sp <sean.parker@x-labsystems.co.uk>
2021-08-16 11:48:41 +0100

    Removed Lazy loading virtual property

---
+++ FullStackMVCAPP/FullStackMVCAPP/Migrations/202108161032381_Using-Eager-Loading.cs
@@ -0,0 +1,16 @@
+namespace FullStackMVCAPP.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UsingEagerLoading : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
