2640763a6ef10a7c06a0d3692107146f7746e902
sc17sp <sean.parker@x-labsystems.co.uk>
2021-08-16 10:42:53 +0100

    Genertaed House controller with read/write scaffolding

---
+++ FullStackMVCAPP/FullStackMVCAPP/Controllers/HouseController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FullStackMVCAPP.Controllers
+{
+    public class HouseController : Controller
+    {
+        // GET: House
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: House/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: House/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: House/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: House/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: House/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: House/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: House/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
