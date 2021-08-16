adf1a889ab9d2c39b77a0fecd1cbd4ef2d2e4e0c
sc17sp <sean.parker@x-labsystems.co.uk>
2021-08-16 10:42:33 +0100

    Added House Route and url parameters

--- FullStackMVCAPP/FullStackMVCAPP/App_Start/RouteConfig.cs
+++ FullStackMVCAPP/FullStackMVCAPP/App_Start/RouteConfig.cs
@@ -18,6 +18,12 @@ namespace FullStackMVCAPP
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                name: "House",
+                url: "Houses/{id}",
+                defaults: new { controller = "House", action = "Index", id = UrlParameter.Optional }
+                );
         }
     }
 }
