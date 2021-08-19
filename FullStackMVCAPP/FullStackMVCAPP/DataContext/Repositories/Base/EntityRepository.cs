97f87366db8a3041fdc008c1be18fc58f6eb252e
Sean Parker <sean.parker@x-labsystems.co.uk>
2021-08-19 13:46:58 +0100

    Added EntityRepostiry class with the list methods and GetById method

---
+++ FullStackMVCAPP/FullStackMVCAPP/DataContext/Repositories/Base/EntityRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using FullStackMVCAPP.Repositories.Base;
+public class EntityRepositories <T> : IEntityRepository <T> where T: class 
+{
+    protected readonly GOTContext _GOTContext;
+
+    public EntityRepositories(GOTContext gameOfThronesContext)
+    {
+        _GOTContext = gameOfThronesContext;
+    }
+
+    public IList<T> EntityList()
+    {
+        return _GOTContext.Set<T>.ToList();
+    }
+
+    public T GetEntityById(int id)
+    {
+        return _GOTContext.Set<T>.Where(ent => ent.Id == id);
+    }
+}
