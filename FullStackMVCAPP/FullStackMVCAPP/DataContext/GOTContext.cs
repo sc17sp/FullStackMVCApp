af856909c8b602d3220212205a0762a006e84c9a
Sean Parker <sean.parker@x-labsystems.co.uk>
2021-08-19 11:34:20 +0100

    Moved GOT Context to its own Class

---
+++ FullStackMVCAPP/FullStackMVCAPP/DataContext/GOTContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Data.Emtity;
+
+public class GOTContext : DbContext 
+{
+	public GOTContext(DbContextOptions <GOTContext> options): base(options)
+	{
+	}
+
+    public DbSet<FullStackMVCAPP.Models.House> Houses { get; set; }
+    public DbSet<FullStackMVCAPP.Models.Castle> Castles { get; set; }
+    public DbSet<FullStackMVCAPP.Models.Character> Characters { get; set; }
+    
+}
