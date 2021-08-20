581e478a9fb31b03546f8873f1728ea953a5db4c
Sean Parker <sean.parker@x-labsystems.co.uk>
2021-08-20 13:35:25 +0100

    Created HouseRepo implementing IEntity

---
+++ FullStackMVCAPP/FullStackMVCAPP/DataContext/Repositories/HouseRepositroy.cs
@@ -0,0 +1,21 @@
+﻿using FullStackMVCAPP.Repositories.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FullStackMVCAPP.DataContext.Repositories
+{
+    public class HouseRepositroy : IEntityRepository <FullStackMVCAPP.Models.House>
+    {
+        public IList<FullStackMVCAPP.Models.House> EntityList(GOTContext _GOTContext)
+        {
+            return _GOTContext.Houses.ToList();
+        }
+
+        public FullStackMVCAPP.Models.House GetEntityByID(GOTContext _GOTContext, int id)
+        {
+            return _GOTContext.Houses.Where(chr => chr.Id == id).First();
+        }
+    }
+}
