e0a4b31f6e4df03d2704b30046472755416f4d92
Sean Parker <sean.parker@x-labsystems.co.uk>
2021-08-20 13:38:29 +0100

    Created individual services for each repo

---
+++ FullStackMVCAPP/FullStackMVCAPP/Services/CharacterService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FullStackMVCAPP.Services
+{
+    public class CharacterService
+    {
+    }
+}
