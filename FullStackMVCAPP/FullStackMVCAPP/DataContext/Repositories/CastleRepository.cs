218ed881ababac2f979dbe1e92f39e449829f89c
Sean Parker <sean.parker@x-labsystems.co.uk>
2021-08-20 13:36:41 +0100

    Implemented CastleRepo that implements IEntityRepo

---
+++ FullStackMVCAPP/FullStackMVCAPP/DataContext/Repositories/CastleRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FullStackMVCAPP.DataContext.Repositories
+{
+    public class CastleRepository
+    {
+        public IList<FullStackMVCAPP.Models.Castle> EntityList(GOTContext _GOTContext)
+        {
+            return _GOTContext.Castles.ToList();
+        }
+
+        public FullStackMVCAPP.Models.Castle GetEntityByID(GOTContext _GOTContext, int id)
+        {
+            return _GOTContext.Castles.Where(chr => chr.Id == id).First();
+        }
+    }
+}
