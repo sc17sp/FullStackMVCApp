33899ab38ac9de9b408a3e5fd0884d85898be146
Sean Parker <sean.parker@x-labsystems.co.uk>
2021-08-20 13:47:25 +0100

    Character service passes context to repositry and calls repo functions to each method

--- FullStackMVCAPP/FullStackMVCAPP/Services/CharacterService.cs
+++ FullStackMVCAPP/FullStackMVCAPP/Services/CharacterService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FullStackMVCAPP.DataContext;
+using FullStackMVCAPP.DataContext.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +9,28 @@ namespace FullStackMVCAPP.Services
 {
     public class CharacterService
     {
+        protected readonly GOTContext _GOTContext;
+        protected readonly CharacterRepository _CharacterRepository;
+
+        public CharacterService(GOTContext gOTContext, CharacterRepository characterRepository)
+        {
+            _GOTContext = gOTContext;
+            _CharacterRepository = characterRepository;
+        }
+
+        public IList<FullStackMVCAPP.Models.Character> GetCharacters()
+        {
+            return _CharacterRepository.EntityList(_GOTContext);    
+        }
+
+        public FullStackMVCAPP.Models.Character GetCharacterById(int id)
+        {
+            return _CharacterRepository.GetEntityByID(_GOTContext, id);
+        }
+
+        public IList<FullStackMVCAPP.Models.Character> GetCharacterByHouseId(int id)
+        {
+            return _CharacterRepository.GetCharacterByHouseId(_GOTContext, id);
+        }
     }
 }
