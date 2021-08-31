bf4a04106a870bd6c5dce660a515994b979a9c23
Sean Parker <sean.parker@x-labsystems.co.uk>
2021-08-31 14:27:48 +0100

    Made Castle context virtual to operate with unit test

--- FullStackMVCAPP/FullStackMVCAPP/DataContext/GOTContext.cs
+++ FullStackMVCAPP/FullStackMVCAPP/DataContext/GOTContext.cs
@@ -7,7 +7,7 @@ public class GOTContext : DbContext
 {
     
     public DbSet<FullStackMVCAPP.Models.House> Houses { get; set; }
-    public DbSet<FullStackMVCAPP.Models.Castle> Castles { get; set; }
+    public virtual DbSet<FullStackMVCAPP.Models.Castle> Castles { get; set; }
     public DbSet<FullStackMVCAPP.Models.Character> Characters { get; set; }
     
 }
