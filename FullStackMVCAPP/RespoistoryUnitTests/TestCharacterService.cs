674806838227bfaa6161f6deb27d750cebbdfdd9
sc17sp <sean.parker@x-labsystems.co.uk>
2021-09-01 13:58:33 +0100

    Character Service unoit test added for each service method

---
+++ FullStackMVCAPP/RespoistoryUnitTests/TestCharacterService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RespoistoryUnitTests
+{
+    [TestClass]
+    public class TestCharacterService
+    {
+        [TestMethod]
+        public void TestGetCharacters()
+        {
+        }
+
+        public void TestGetCharactersById()
+        {
+        }
+
+        public void TestGetCharactersByHouseId()
+        {
+        }
+    }
+}
