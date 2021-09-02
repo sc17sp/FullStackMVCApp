c7860e5563251b5fc4f60cc5efbd6ad3a0c055b4
sc17sp <sean.parker@x-labsystems.co.uk>
2021-09-02 16:44:39 +0100

    Fixed entity list Unit test

--- FullStackMVCAPP/RespoistoryUnitTests/TestCharacterRepository.cs
+++ FullStackMVCAPP/RespoistoryUnitTests/TestCharacterRepository.cs
@@ -16,14 +16,22 @@ namespace RespoistoryUnitTests
         public Mock<GOTContext> _MockGOTContext;
         public Mock<DbSet<Character>> _MockCharacterSet;
         public Mock<DbSet<House>> _MockHouseSet;
-        public IQueryable<House> _TestHouseData;
-        public IQueryable<Character> _TestCharacterData;
 
 
         [TestInitialize]
         public void SetUp()
         {   
+            _MockGOTContext = new Mock<GOTContext>();
+            _MockCharacterSet = new Mock<DbSet<Character>>();
+            _MockHouseSet = new Mock<DbSet<House>>();
+        }
+
+        [TestMethod]
+        public void TestEntityList()
+        {   
             //Arrange
+            SetUp();
+
             var testDataHouse = new List<House>
             {
                 new House{ Id=1, Name="Stark", Region="The North", Words="Winter is coming"},
@@ -31,36 +39,26 @@ namespace RespoistoryUnitTests
                 new House{ Id=3, Name="Grey Joy", Region="The Iron Islands", Words="What is dead my never die"}
             }.AsQueryable();
 
-            _TestHouseData = testDataHouse;
-
             var testDataCharacters = new List<Character>
             {
-                new Character{Id=1, FirstName="Jon", LastName = "Snow", Alive=true, House=_TestHouseData.First()},
-                new Character{Id=2, FirstName="Jamie", LastName = "Lannister", Alive=false, House=_TestHouseData.ElementAt(1)},
-                new Character{Id=3, FirstName="Theon", LastName = "Greyjoy", Alive=false, House=_TestHouseData.Last()}
+                new Character{Id=1, FirstName="Jon", LastName = "Snow", Alive=true, House=testDataHouse.First()},
+                new Character{Id=2, FirstName="Jamie", LastName = "Lannister", Alive=false, House=testDataHouse.ElementAt(1)},
+                new Character{Id=3, FirstName="Theon", LastName = "Greyjoy", Alive=false, House=testDataHouse.Last()}
             }.AsQueryable();
 
-            _TestCharacterData = testDataCharacters;
 
-            _MockGOTContext = new Mock<GOTContext>();
-            _MockCharacterSet = new Mock<DbSet<Character>>();
-            _MockHouseSet = new Mock<DbSet<House>>();
+            _MockHouseSet.As<IQueryable<House>>().Setup(m => m.Provider).Returns(testDataHouse.Provider);
+            _MockHouseSet.As<IQueryable<House>>().Setup(m => m.Expression).Returns(testDataHouse.Expression);
+            _MockHouseSet.As<IQueryable<House>>().Setup(m => m.ElementType).Returns(testDataHouse.ElementType);
+            _MockHouseSet.As<IQueryable<House>>().Setup(m => m.GetEnumerator()).Returns(testDataHouse.GetEnumerator());
 
-            _MockHouseSet.As<IQueryable<House>>().Setup(m => m.Provider).Returns(_TestHouseData.Provider);
-            _MockHouseSet.As<IQueryable<House>>().Setup(m => m.Expression).Returns(_TestHouseData.Expression);
-            _MockHouseSet.As<IQueryable<House>>().Setup(m => m.ElementType).Returns(_TestHouseData.ElementType);
-            _MockHouseSet.As<IQueryable<House>>().Setup(m => m.GetEnumerator()).Returns(_TestHouseData.GetEnumerator());
+            _MockCharacterSet.As<IQueryable<Character>>().Setup(m => m.Provider).Returns(testDataCharacters.Provider);
+            _MockCharacterSet.As<IQueryable<Character>>().Setup(m => m.Expression).Returns(testDataCharacters.Expression);
+            _MockCharacterSet.As<IQueryable<Character>>().Setup(m => m.ElementType).Returns(testDataCharacters.ElementType);
+            _MockCharacterSet.As<IQueryable<Character>>().Setup(m => m.GetEnumerator()).Returns(testDataCharacters.GetEnumerator());
 
-            _MockCharacterSet.As<IQueryable<Character>>().Setup(m => m.Provider).Returns(_TestCharacterData.Provider);
-            _MockCharacterSet.As<IQueryable<Character>>().Setup(m => m.Expression).Returns(_TestCharacterData.Expression);
-            _MockCharacterSet.As<IQueryable<Character>>().Setup(m => m.ElementType).Returns(_TestCharacterData.ElementType);
-            _MockCharacterSet.As<IQueryable<Character>>().Setup(m => m.GetEnumerator()).Returns(_TestCharacterData.GetEnumerator());
-        }
-        [TestMethod]
-        public void TestEntityList()
-        {   
-            //Arrange
-            SetUp();
+            _MockGOTContext.Setup(h => h.Houses).Returns(_MockHouseSet.Object);
+            _MockGOTContext.Setup(c => c.Characters).Returns(_MockCharacterSet.Object);
             //Act
             var characterRepository = new CharacterRepository();
             var listOfCharacters = characterRepository.EntityList(_MockGOTContext.Object);
